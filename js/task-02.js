7d930572d9a3aabd9124341cdf2bd1b7245f1704
SvistPetro <svist.petro@gmail.com>
2023-04-18 22:27:11 +0300

    Task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsUl = document.querySelector("#ingredients");
+
+const ingredientsArray = [];
+
+ingredients.forEach(ingredient => {
+  const ingredientElement = document.createElement("li");
+
+  ingredientElement.textContent = ingredient;
+  ingredientElement.classList = "item";
+
+  ingredientsArray.push(ingredientElement);
+});
+
+ingredientsUl.append(...ingredientsArray);
