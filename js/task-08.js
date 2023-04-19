4115c8e8fbf093999177622ae93a85f089ad0ee8
SvistPetro <svist.petro@gmail.com>
2023-04-20 01:18:17 +0300

    Task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+
+  const formData = {
+    mail,
+    password,
+  };
+    
+    if (mail === "" || password === "") {
+      return alert("Всі поля повинні бути заповнені!");
+    }
+
+    console.log(formData);
+    form.reset();
+};
