767dda6e0fc61f4261f85cfb0da34f527f480293
SvistPetro <svist.petro@gmail.com>
2023-04-20 01:34:02 +0300

    Task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,17 @@
+
+const bodyEl = document.querySelector("body");
+const buttonEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+
+buttonEl.addEventListener("click", changeColor);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
+
+function changeColor(event) {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+};
